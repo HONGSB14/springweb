91dad5f68ffeea212b19e39f252fd0bef8641a4a
HONGSB14 <HOSNSB14@naver.com>
2022-06-16 10:37:17 +0900

    Changes

--- src/main/java/hongweb/controller/MemberController.java
+++ src/main/java/hongweb/controller/MemberController.java
@@ -5,9 +5,12 @@ import hongweb.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //주요응답/요청
+@RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/login")
@@ -15,13 +18,16 @@ public class MemberController {
         return "login";
     }
 
+    @GetMapping("/signup")
+    public String signup(){
+        return "/member/write";
+    }
 
     @Autowired  //자동 빈(메모리) 생성
     MemberService memberService;
 
-    @GetMapping("/signup")
-    @ResponseBody
-    public String signup(MemberDto  memberDto){
+    @PostMapping("/signup")
+    public String save(MemberDto  memberDto){
 
             //서비스 호출
         memberService.signup(memberDto);
